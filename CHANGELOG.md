d7b56f791be11c6ddd736a908bcf053cf0ea61df
Waleed Dahshan <waleedmast@gmail.com>
2022-06-11 23:28:16 +1000

    enable context to store other values than booleans

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,7 @@
 # Change Log
 
-All notable changes to the "contextually" extension will be documented in this file.
-
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
 ## [Unreleased]
 
 - Initial release
+
+- Made a masterful app
--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,14 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// a mapping from when clause contexts to whether they are active
-// necessary because getContext is not available by vscode.
+// a mapping from when clause contexts names to their values. And other things "owned" by
+// them. For example, storing the statusbar item so it can be updated.
+// necessary because getContext values are not available by vscode.
 interface ContextList {
-	[context: string]: boolean
+	[context: string]: {
+		value: any,
+		status: vscode.StatusBarItem,
+	}
 }
 
 // start up the extension!
@@ -20,20 +24,33 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// initialize each context as false, and add `turnOn` `turnOff` and `toggle`
 	// commands for each context.
-
 	for (let context of contexts || []) {
-		map[context] = false;
+		map[context] = {
+			value: false,
+			status: vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, -2),
+		};
+
+		// initialize context value
 		vscode.commands.executeCommand('setContext', context, false);
 
-		// add ability to turn on context.
+		// introduce a status bar item.
+		map[context].status.backgroundColor = new vscode.ThemeColor("statusBarItem.warningBackground");
+		map[context].status.color = "#ffffff";
+		changeStatus(map, context);
+		map[context].status.tooltip = `${context}`;
+		map[context].status.show();
+
+		// add a command that turns on to turn on context.
 		vscode.commands.registerCommand(`contextually.turnOn${context}`, () => {
-			map[context] = true;
+			map[context].value = true;
 			vscode.commands.executeCommand('setContext', context, true);
 		});
 
 		// add ability to turn off context.
 		vscode.commands.registerCommand(`contextually.turnOff${context}`, () => {
-			map[context] = false;
+			map[context].value = false;
+			changeStatus(map, context);
+
 			vscode.commands.executeCommand('setContext', context, false);
 		});
 
@@ -41,15 +58,31 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand(`contextually.toggle${context}`, () => {
 			console.log(`toggled ${context}`);
 			// invert the context.
-			map[context] = !map[context];
-			console.log(`${context} is ${map[context]}`);
+			map[context].value = !map[context].value;
+			changeStatus(map, context);
+
 			vscode.commands.executeCommand(
 				'setContext',
 				context,
 				map[context]
 			);
 		});
+
 	}
+
+	// add a generic command to change a context to a certain value.
+	vscode.commands.registerCommand(`contextually.updateContext`, ([name , value]) => {
+		if (map[name] !== undefined) {
+			map[name].value = value;
+			changeStatus(map, name);
+		}
+	});
+}
+
+// change status-bar item text.
+function changeStatus(map: ContextList, context: string): void {
+	// todo: maybe control width.
+	map[context].status.text = `${context}: ${map[context].value}`;
 }
 
 // this method is called when your extension is deactivated
